de5c66245d1cc48953bb0824db653c7731af9451
Saurabh <saurabh20tiwari04@gmail.com>
2026-06-19 23:22:23 +0530

    switch case in golang

---
+++ switch/switch.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+
+func main(){
+
+	i := 2
+	fmt.Println("write ", i, "as")
+	switch i {
+		case 1:
+			fmt.Println("one")
+		case 2:
+			fmt.Println("two")
+		case 3:
+			fmt.Println("three")
+	}
+
+	switch time.Now().Weekday(){
+		case time.Saturday, time.Sunday:
+			fmt.Println("It is the weekend")
+		default:
+			fmt.Println("It's the weekday")
+	}
+
+	t := time.Now()
+	switch {
+		case t.Hour() < 12:
+		fmt.Println("It's the before noon")
+		default:
+		fmt.Println("It's after noon")
+	}
+
+	WhatAmI := func(i interface{}{
+	    	switch t := i.(type){
+			case bool:
+				fmt.Println("I'm a bool")
+			case int:
+				fmt.Println("I'm an int")
+			default:
+				fmt.Printf("Don't know type %\n", t)
+		}
+	}
+	WhatAmI(true)
+	WhatAmI(1)
+	WhatAmI("hey")	
+}
