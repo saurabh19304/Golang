628e1919c2f2473b46451475436d62c828ee1414
Saurabh <saurabh20tiwari04@gmail.com>
2026-06-22 23:16:19 +0530

    learning the slices

--- array/array.go
+++ array/array.go
@@ -22,7 +22,6 @@ func main() {
 	b = [...]int{100, 3: 200, 4}
 	fmt.Println("ind", b)
 
-
 	var twoD [2][3]int
 	for i := range 2 {
 		for j := range 3 {
@@ -32,8 +31,8 @@ func main() {
 	fmt.Println("2d:", twoD)
 
 	twoD = [2][3]int{
-		{1,2,3},
-		{1.2.3},
+		{1, 2, 3},
+		{1, 2, 3},
 	}
 	fmt.Println("2d", twoD)
 }
---
+++ slices/slices.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	var s []string
+	fmt.Println("uninit:", s, s == nil, len(s) == 0)
+
+	s = make([]string, 3)
+	fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
+
+}
